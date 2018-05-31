3b91d310e053f21e0c016e42f59dd5e458cd674f
Jason Rubenstein <jason@Jasons-MacBook-Air.local>
2018-05-30 20:16:37 -0400

    run prettier

--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import withStyles from './index'
+import React from 'react';
+import withStyles from './index';
 import { mount } from 'enzyme';
 
 describe('with-styles', () => {
-  it("should render component with styles when initialized with object", () => {
+  it('should render component with styles when initialized with object', () => {
     const Component = withStyles({
       div: {
         backgroundColor: '#000'
       }
-    })(({styles}) => <div style={styles.div}/>);
+    })(({ styles }) => <div style={styles.div} />);
 
-    const MountedComponent = mount(<Component/>)
+    const MountedComponent = mount(<Component />);
 
     expect(MountedComponent).toMatchSnapshot();
-  })
+  });
 
-  it("should render component with styles when initialized with object", () => {
-    const Component = withStyles(({backgroundColor}) => ({
+  it('should render component with styles when initialized with object', () => {
+    const Component = withStyles(({ backgroundColor }) => ({
       div: {
         backgroundColor
       }
-    }))(({styles}) => <div style={styles.div}/>);
+    }))(({ styles }) => <div style={styles.div} />);
 
-    const MountedComponent = mount(<Component backgroundColor="#fff"/>)
+    const MountedComponent = mount(<Component backgroundColor="#fff" />);
 
     expect(MountedComponent).toMatchSnapshot();
-  })
-})
+  });
+});
